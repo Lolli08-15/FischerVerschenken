f5ea1de1f0799b0fcae73a42cd89b552f673e0a9
xJanise <twobrotherssing@gmail.com>
2024-10-17 12:08:13 +0200

    ai basic functionality, add TODO to txt

--- ai.py
+++ ai.py
@@ -1,2 +1,14 @@
-def ai_think(shot_List):
-    exit
+import random
+
+freeSquares = []            #generate possible squares, to keep track of shots
+for i in range(10):
+    for j in range(10):
+        freeSquares.append([i,j])   
+
+
+def ai_shoot(response):     #input is whether the last shot missed(0), hit(1) or sunk(2) 
+    
+    pickedSquare = random.choice(freeSquares)
+    aimX, aimY = pickedSquare
+    freeSquares.remove(pickedSquare)
+    return aimX, aimY       #returns aim coordinates 
