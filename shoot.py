671c30af21ad896f1d969bfdb84925834fcab4c6
Kyucy-Qc <bjarne.siehl@gmail.com>
2024-10-18 08:28:24 +0200

    posXY change!!!

--- shoot.py
+++ shoot.py
@@ -1,6 +1,4 @@
-def shoot(player, aimX, aimY):
-
-    aimXY = aimX*10+aimY # füge X udn Y in eine Var zusammen
+def shoot(player, aimXY):
  
     if aimXY in player.shotList: # wenn dieser latz bereicht beschossen wurde
         return False # gebe False zurück
@@ -10,13 +8,13 @@ def shoot(player, aimX, aimY):
         if aimXY in fish.occupied: # wenn an der stelle des schusses ein Fisch ist
             fish.hits += 1 # hitte den Fisch
             if fish.length == fish.hits: # wenn getroffener Fisch hit anzahl größer als Fishlänge, dann setze gebe "sunk" aus
-                player.addShot(aimX, aimY, "sunk") # führe einen Speichervorgang vom Schuss zu shotList aus
+                player.addShot(aimXY, "sunk") # führe einen Speichervorgang vom Schuss zu shotList aus
                 return "sunk" # gebe "sunk zurück"
 
             else: # sonst gebe hit aus
-                player.addShot(aimX, aimY, "hit") # führe einen Speichervorgang vom Schuss zu shotList aus
+                player.addShot(aimXY, "hit") # führe einen Speichervorgang vom Schuss zu shotList aus
                 return "hit" # gebe "hit" zurück
 
-    player.addShot(aimX, aimY, "miss") # führe einen Speichervorgang vom Schuss zu shotList aus
+    player.addShot(aimXY, "miss") # führe einen Speichervorgang vom Schuss zu shotList aus
     return "miss" # gebe "miss" zurück
         
