5e40c6c00f410df893fcfc280ef60524dcbeb46c
xJanise <twobrotherssing@gmail.com>
2024-10-18 08:51:51 +0200

    parallel ship handling, start

--- ai.py
+++ ai.py
@@ -7,6 +7,7 @@ prevMiss = 0
 hitShots = set()                #list of shots that hit
 recentShots = list()            #list of recent shots that hit (order important)
 targetShots = set()             #squares to target after getting a hit
+directionS = set()              #directionally important coords
 freeSquares = set()             #generate possible squares, to keep track of shots
 for x in range(10):
     for y in range(10):
@@ -34,27 +35,37 @@ def aiAim(response):     #input is whether the last shot missed(0), hit(1) or su
             targetShots.add(tuple(map(add, hits, (1,0))))
             targetShots.add(tuple(map(add, hits, (0,1))))
             targetShots.add(tuple(map(add, hits, (-1,0))))  #add all surrounding squares
-        targetShots &= freeSquares                          #remove previous shots/oob
         return aiShoot(targetShots)
     
     elif(notSunk == 1):
         if (response == 1):                                 #if hit, go that, or the other way
             hitShots.add(pickedSquare)
             recentShots.append(pickedSquare)
-            directionX, directionY=tuple(map(sub, recentShots[-2], recentShots[-1]))                   #calculate direction
+            directionX, directionY=tuple(map(sub, recentShots[0], recentShots[-1]))                   #calculate direction
+            targetShots.clear()
+            '''targetShots.add(tuple(map(add, recentShots[-1], (directionX, directionY))))
+            targetShots.add(tuple(map(add, recentShots[0], (-directionX, -directionY))))'''
+            directionS = set((directionX,directionY),(-directionX,-directionY))
+            for each in directionS:
+                while True:
+                    #check up and down     if alredy miss??
+                    if('''ende'''):
+                        break
+            aiShoot(targetShots)
             
-        else:                                               #otherwise try other direction
+        else:                                               #if miss try other direction
             if(prevMiss == 0):
                 prevMiss = 1
             else:
+                eeeeyup = len(recentShots)                  #RENAME  KEEPS TRACK OF SUNK SHIP SIZE
                 pass#
             
     else:
         return aiShoot(freeSquares)
 
 def aiShoot(aim):
-    global pickedSquare
-    targetSquares = list(aim)
+    global pickedSquare                              
+    targetSquares = freeSquares & list(aim)         #remove previous shots/oob
     pickedSquare = random.choice(targetSquares)     #pick a square based on the choice
     aimX, aimY = pickedSquare
     freeSquares.remove(pickedSquare)
