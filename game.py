7fa1948cce55b47e71829912ecbb04f5279f750e
pietbauer <103959736+pietbauer@users.noreply.github.com>
2024-10-21 10:02:25 +0200

    Player Shoot function inside Game.py

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ class Player:
     def addFish(self, fish):
         self.fishes.append(fish)
         
-    def removeFish(self,posXY):
+    def removeFish(self, posXY):
         for fish in self.fishes:
             #print(fish.occupied)
             if posXY in fish.occupied:
@@ -19,8 +19,6 @@ class Player:
                 return length
         return 0
                 
-    def addShot(self, posXY, what):
-        self.shotList.append([posXY, what])
 
     def getSunkenFish(self):
         count = 0
@@ -54,8 +52,6 @@ class Game:
         return False
     
 
-    def placeFish(self,posXY,direction,length):
-        return place(self.player1,posXY,direction,length)
 
 
     def placeAiFish(self):
@@ -67,12 +63,8 @@ class Game:
         place(self.ai, (0, 4), 1, 5)
     
 
-    def removeFish(self, posXY):
-        return self.player1.removeFish(posXY)
     
 
-    def playerShoot(self, posXY):
-        return shoot(self.player1, posXY)
     
 
     def getSunkenFish(self, player):
@@ -111,13 +103,10 @@ class Game:
 
 """_____________________Test Start_____________________"""
 if __name__ == "__main__":
-    game = Game()
-    trash = game.player1.removeFish((1,0))
+    games = Game()
+    trash = games.player1.removeFish(1,0)
     print(trash)
-    game.player1.showOff()
-    game.placeFish((0, 0), 1, 4)
-
-    print(shoot(game.player1, (0, 0)))
+    games.player1.showOff()
 """_____________________Test End______________________"""
 
 """
