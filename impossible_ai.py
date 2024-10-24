94b04eff2158aefe7f4be04855aad8dbb558a8f4
lolli0815 <o.altnau@icloud.com>
2024-10-24 14:37:03 +0200

    global added to impossible ai

--- impossible_ai.py
+++ impossible_ai.py
@@ -1,16 +1,19 @@
 shotlist = []
 shots = -1
 def initAI(player):
+    global shotlist, shots
     for fish in player.fishes:
         for slot in fish.occupied:
             shotlist.append(slot)
 
 def shootAI(player):
+    global shotlist, shots
     if len(shotlist) < 1:
         initAI(player)
     shots +=1
     return shotlist[shots]
 
 def resetAI():
+    global shotlist, shots
     shotlist = []
     shots=-1
