d00af70e73cf5fbbf20703729d04b14d4cb0d13b
lolli0815 <o.altnau@icloud.com>
2024-10-24 13:59:23 +0200

    diffrent Fish lengths

--- settings.py
+++ settings.py
@@ -1,10 +1,16 @@
 width = 1600
 height = 900
 
-ai_mode = True
+ai_mode = False
 
 ai_processing_time = 30 * 1.5 # 1.5 Seconds
 if ai_mode:
     ai_processing_time = 2 # 2 Frames
 
-fish_lengths = [2, 3, 3, 4, 5]
+fish_lengths0 = [2, 3, 3, 4, 5] #Standart Array
+fish_lengths1 = [3, 4, 4, 3, 5] #Fast Round
+fish_lengths2 = [2, 2, 3, 3, 3, 4, 4] #slow Round
+fish_lengths3 = [2, 2, 2, 2, 2, 2] #only 2er
+fish_lengths4 = [3, 3, 3, 3, 3] #only 3er
+fish_lengths5 = [4, 4, 4, 4] #only 4er
+fish_lengths6 = [5, 5, 5] #only 5er
