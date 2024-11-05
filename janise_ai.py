f8563585756e911dd330179b312eb8d766ebbedd
xJanise <twobrotherssing@gmail.com>
2024-11-05 11:43:21 +0100

    fixed rare bug, which occured when a cluster was surrounded

--- janise_ai.py
+++ janise_ai.py
@@ -3,13 +3,14 @@ from operator import add
 
 
 def resetAI():
-    global hit, recentHits, freeSquares, gridSquares
+    global hit, recentHits, freeSquares, gridSquares, targetShots
     
     gridCounter = random.randint(0,2)
     hit     = 0     #resolving hits
     recentHits = list()             #list of shots since the last sink
     freeSquares = set()             #generate possible squares, to keep track of shots
     gridSquares = set()             #squares to shoot if not aiming
+    targetShots = set()             #squares to shoot if yes aiming
     for x in range(10):
         gridCounter += 1
         for y in range(10):
@@ -35,13 +36,17 @@ def shootAI(response):
             targetShots.add(tuple(map(add, each, (0,1))))   #add adjacents as targets
             targetShots.add(tuple(map(add, each, (-1,0))))   #add adjacents as targets
         targetShots &= freeSquares
+        if not targetShots:             #exit cleanup
+            hit=0
+            recentHits.clear
+            return aiShoot(gridSquares)
         return aiShoot(targetShots)                                 #fire
     elif hit == 1:                      #miss after hit
         targetShots &= freeSquares
         if not targetShots:             #exit cleanup
             hit=0
             recentHits.clear
-            return aiShoot(freeSquares)
+            return aiShoot(gridSquares)
         return aiShoot(targetShots)                                 #fire
         
         
