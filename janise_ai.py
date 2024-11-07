106b7a9ddd8015f8ceb54bdb3c65143dc501e646
xJanise <twobrotherssing@gmail.com>
2024-11-07 09:57:38 +0100

    commented and restructured all my AIs

--- janise_ai.py
+++ janise_ai.py
@@ -4,7 +4,6 @@ from operator import add
 
 def resetAI():
     global hit, recentHits, freeSquares, gridSquares, targetShots
-    
     gridCounter = random.randint(0,2)
     hit     = 0     #resolving hits
     recentHits = list()             #list of shots since the last sink
@@ -21,15 +20,13 @@ def resetAI():
         
 def clearAim():     #clear up and shoot at grid
     global hit, gridSquares
-    hit=0
+    hit=0                   #reset Vars
     recentHits.clear
-    gridSquares &= freeSquares
-    if not gridSquares:
+    gridSquares &= freeSquares      #remove hit squares from grid
+    if not gridSquares:     #if grid squares are empty
         gridSquares = freeSquares
     return aiShoot(gridSquares)
-    
-        
-        
+     
 def shootAI(response):
     global hit, targetShots, gridSquares
     if response == 0 and hit == 0:      #misses
@@ -43,42 +40,16 @@ def shootAI(response):
             targetShots.add(tuple(map(add, each, (1,0))))       #right
             targetShots.add(tuple(map(add, each, (0,1))))       #down
             targetShots.add(tuple(map(add, each, (-1,0))))      #left
-        targetShots &= freeSquares
-        if not targetShots:             
-            return clearAim()
-        return aiShoot(targetShots)                                 #fire
+        targetShots &= freeSquares      #remove hit squares from grid
+        return clearAim() if not targetShots else aiShoot(targetShots)     #if target shots are empty
     elif hit == 1:                      #miss after hit
-        targetShots &= freeSquares
-        if not targetShots:             
-            return clearAim()
-        return aiShoot(targetShots)                                 #fire
+        targetShots &= freeSquares      #remove hit squares from grid
+        return clearAim() if not targetShots else aiShoot(targetShots)     #if target shots are empty
         
-        
-#the main function
 def aiShoot(aim):
     global pickedSquare   
     pickedSquare = random.choice(list(aim))     #pick a square based on the choice
     aimX, aimY = pickedSquare
-    freeSquares.remove(pickedSquare)
+    freeSquares.remove(pickedSquare)            
     return aimX, aimY     #returns aim coordinates
     
-
-   
-#DEBUG  
- 
-if __name__ == "__main__":
-    '''
-        
-    shotLines.addLine(1,{{5,7},{4,7},{6,7},{7,7}})
-    print(shotLines.lines[0][1]*2)
-    exit'''
-    resetAI()
-    print(f"miss {shootAI(0)}")
-    print(f"hit  {shootAI(0)}")
-    print(f"miss {shootAI(1)}")
-    print(f"hit  {shootAI(0)}")
-    print(f"hit  {shootAI(1)}")
-    print(f"miss {shootAI(1)}")
-    print(f"miss {shootAI(0)}")
-    print(f"miss {shootAI(0)}")
-    print()
--- janise_ai_dirty.py
+++ janise_ai_dirty.py
@@ -4,8 +4,6 @@ from operator import add
 
 def resetAI():
     global hit, recentHits, freeSquares, gridSquares, targetShots, shotToSink, alltheHits
-    
-    
     gridCounter = random.randint(0,2)
     hit     = 0     #resolving hits
     recentHits = list()             #list of shots since the last sink
@@ -24,24 +22,22 @@ def resetAI():
         
 def clearAim():     #clear up and shoot at grid
     global hit, gridSquares
-    hit=0
+    hit=0                           #reset vars
     recentHits.clear()
     targetShots.clear()
-    gridSquares &= freeSquares
-    if not gridSquares:
-        for each in alltheHits: 
+    gridSquares &= freeSquares      #remove hit squares from grid
+    if not gridSquares:             #if gridSquares are empty
+        for each in alltheHits:     #check squares surrounding hits
             gridSquares.add(tuple(map(add, each, (0,-1))))      #up
             gridSquares.add(tuple(map(add, each, (1,0))))       #right
             gridSquares.add(tuple(map(add, each, (0,1))))       #down
             gridSquares.add(tuple(map(add, each, (-1,0))))      #left
-        gridSquares &= freeSquares
-        if not gridSquares:
-            gridSquares = freeSquares
+        gridSquares &= freeSquares  #remvoe hit squares
+        if not gridSquares:         #if still empty
+            gridSquares = freeSquares   #shoot at remaining squares
         
     return aiShoot(gridSquares)
-    
-        
-        
+ 
 def shootAI(response):
     global hit, targetShots, gridSquares
     if response==1: 
@@ -56,35 +52,24 @@ def shootAI(response):
                 targetShots.add(tuple(map(add, each, (0,1))))       #down
                 targetShots.add(tuple(map(add, each, (-1,0))))      #left
             targetShots &= freeSquares
-            if not targetShots:
-                return clearAim()
-            else:
-                return aiShoot(targetShots)
+            return clearAim() if not targetShots else aiShoot(targetShots)     #if target shots are empty
         elif hit > 1:
             ax, ay = recentHits[0]
             bx, by = recentHits[1]
-            dx, dy = (ax-bx,ay-by)
+            dx, dy = (ax-bx,ay-by)      #get direction (difference recent 1 to 0)
             for each in recentHits:
-                targetShots.add(tuple(map(add, each, (dx, dy))))
-                targetShots.add(tuple(map(add, each, (-dx, -dy))))
-            targetShots &= freeSquares
-            if not targetShots:
-                return clearAim()
-            else:
-                return aiShoot(targetShots)
+                targetShots.add(tuple(map(add, each, (dx, dy))))    #add direction one way
+                targetShots.add(tuple(map(add, each, (-dx, -dy))))  #add the other way
+            targetShots &= freeSquares  #removes previously hit squares
+            return clearAim() if not targetShots else aiShoot(targetShots)     #if target shots are empty
         print("uh oh")
         pass
     elif response == 0:         #if miss
         targetShots &= freeSquares
-        if not targetShots:
-            return clearAim()
-        else:
-            return aiShoot(targetShots)
-    else:
+        return clearAim() if not targetShots else aiShoot(targetShots)     #if target shots are empty
+    else:                       #if sink
         return clearAim()
         
-        
-#the main function
 def aiShoot(aim):
     global pickedSquare   
     pickedSquare = random.choice(list(aim))     #pick a square based on the choice
